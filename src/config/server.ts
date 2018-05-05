4a4dd120e4bcc4b4dd84ff4beac8a4cafcd7ba25
Luciano Mattos <desenv.mattos@hotmail.com>
2018-05-04 21:09:36 -0300

    Inicializando conexão com o mongodb

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,10 +1,18 @@
 import * as express from 'express';
+import * as mongoose from 'mongoose';
+
+import { environment } from '../common/environment';
 
 class Server {
     public express: express.Application;
 
     constructor() {
         this.express = express();
+        this.initializeDB();
+    }
+
+    initializeDB() {
+        return mongoose.connect(environment.db.url);
     }
 }
 
