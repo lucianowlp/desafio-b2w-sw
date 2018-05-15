2baa245f4c93d815c4f5ce771be900b796d3525f
Luciano Mattos <desenv.mattos@hotmail.com>
2018-05-14 21:54:58 -0300

    Ajustes compilação do projeto

--- src/common/environment.ts
+++ src/common/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   server: { port: process.env.SERVER_PORT || 5005 },
   serverTest: { url: `http://localhost:${process.env.SERVER_PORT || 5005}` },
-  db: { url: process.env.DB_URL || 'mongodb://localhost/swapi' }
+  db: { url: process.env.DB_URL || 'mongodb://localhost/swapi' },
+  urlSWApi: 'http://swapi.co/api/'
 }
